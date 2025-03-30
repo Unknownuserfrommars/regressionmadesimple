dad16786235bab5712d24e085c87f0f6eb8b2d35
Unknownuserfrommars <163658509+Unknownuserfrommars@users.noreply.github.com>
2025-03-30 23:53:04 +0800

    Delete regressionmadesimple directory

--- regressionmadesimple/__init__.py
+++
@@ -1,3 +0,0 @@
-from .linear import Linear
-from .utils_preworks import preworks
-__all__ = ['Linear', 'preworks']
--- regressionmadesimple/base_class.py
+++
@@ -1,9 +0,0 @@
-class BaseModel:
-    def fit(self):
-        raise NotImplementedError
-
-    def predict(self, X):
-        raise NotImplementedError
-
-    def summary(self):
-        raise NotImplementedError
